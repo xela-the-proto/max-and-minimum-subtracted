d6130dfdc3a3e068daf0eaed8955aa78a405f4f3
alex <78649142+xela-the-proto@users.noreply.github.com>
2021-02-12 14:54:54 +0100

    Add files via upload

---
+++ DiminuireMaxDiMin_Funzioni/cmake-build-debug/CMakeFiles/DiminuireMaxDiMin_Funzioni.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiminuireMaxDiMin_Funzioni.dir/funzioni.cpp.obj"
+  "CMakeFiles/DiminuireMaxDiMin_Funzioni.dir/main.cpp.obj"
+  "DiminuireMaxDiMin_Funzioni.exe"
+  "DiminuireMaxDiMin_Funzioni.exe.manifest"
+  "DiminuireMaxDiMin_Funzioni.lib"
+  "DiminuireMaxDiMin_Funzioni.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiminuireMaxDiMin_Funzioni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DiminuireMaxDiMin_Funzioni/cmake-build-debug/CMakeFiles/DiminuireMaxDiMin_Funzioni.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DiminuireMaxDiMin_Funzioni.
+# This may be replaced when dependencies are built.
